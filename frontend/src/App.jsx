b9cfd3a474b93389bbf4ad7e4a08325016ac6520
alex91-html <alex91-html@users.noreply.github.com>
2025-08-13 15:14:52 +0200

    somthing can't remember

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 // import React from 'react';
 import Scene from './pages/3DScene/3DScene';
-
+import Joystick from "./components/ui/Joystick";
 import IdeaCarousel from './pages/ideas/IdeaCarousel/IdeaCarousel';
 
 export const App = () => {
   return (
     <>
+      <Joystick onMove={(data) => console.log(data)} />
       {/* <IdeaCarousel /> */}
       <Scene velocity={0} />
     </>
